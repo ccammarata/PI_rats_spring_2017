c2ee750d59b5bd4f3a6629e988583230919a5f79
Celine Cammarata <cmc495@cornell.edu>
2017-04-03 11:42:52 -0400

    updated PI attempts for session 40-44
    updated

--- attempts_data/attempts.R
+++ attempts_data/attempts.R
@@ -2,14 +2,14 @@
 ratList <- 101:112
 
 #make a data frame for the AB acquisition days
-sessList <- 40:42 #list session nubmers here
+sessList <- 40:44 #list session nubmers here
 x <- length(sessList)
 y <- length(ratList)
 rowsNeeded <- (x*y)
 
 #making an empty matrix
-data1 <- matrix(data = NA, nrow = rowsNeeded, ncol=4)
-colnames(data1)<- c('ratID', 'session', 'firstAttempt','totalAttempts')
+data1 <- matrix(data = NA, nrow = rowsNeeded, ncol=5)
+colnames(data1)<- c('ratID', 'session', 'firstAttempt','meanAttempts','medianAttempts')
 
 i = 1
 for (sessNum in sessList){
@@ -20,7 +20,8 @@ for (sessNum in sessList){
     currentData$resp2 <- ifelse(currentData$Attempts == "1", 1,0)
     firstAttempts <- mean(currentData$resp2)
     totalAttmpts <- mean(currentData$Attempts)
-    data1[i,0:4] <- c(rat, sessNum, firstAttempts,totalAttmpts)
+    MedianAttmpts <- median(currentData$Attempts)
+    data1[i,0:5] <- c(rat, sessNum, firstAttempts,totalAttmpts,MedianAttmpts)
     i=i+1
   }
 }
@@ -28,5 +29,13 @@ for (sessNum in sessList){
 data1 <- data.frame(data1) #making it into a data frame
 data1$ratID <- factor(data1$ratID) #making rat number a factor
 
-aggregate(data1$firstAttempt, by=list(data1$session), FUN='median')
-aggregate(data1$totalAttempts, by=list(data1$session), FUN='median')
+aggregate(data1$firstAttempt, by=list(data1$session), FUN='mean')
+aggregate(data1$firstAttempt, by=list(data1$session), FUN='sd')
+
+aggregate(data1$meanAttempts, by=list(data1$session), FUN='mean')
+aggregate(data1$meanAttempts, by=list(data1$session), FUN='sd')
+
+aggregate(data1$medianAttempts, by=list(data1$session), FUN='mean')
+aggregate(data1$medianAttempts, by=list(data1$session), FUN='sd')
+
+write.csv(data1, file="Group_Attempt_Data.csv")
