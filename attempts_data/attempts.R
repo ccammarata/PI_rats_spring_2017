9f3798e5b07637f327c4058276a87b2578350dd1
Celine Cammarata <cmc495@cornell.edu>
2017-04-24 10:29:42 -0400

    numerous
    numerous changes, mostly adding some excel output

--- attempts_data/attempts.R
+++ attempts_data/attempts.R
@@ -2,7 +2,7 @@
 ratList <- 101:112
 
 #make a data frame for the AB acquisition days
-sessList <-list(45,47,48)#list session nubmers here
+sessList <- 55:57#list session nubmers here
 x <- length(sessList)
 y <- length(ratList)
 rowsNeeded <- (x*y)
@@ -15,13 +15,14 @@ i = 1
 for (sessNum in sessList){
   for (rat in ratList) {
     filename <- paste("U2_unlimi_",rat,"_",sessNum,".csv", sep="")
-    print(filename)
-    currentData <- read.csv(filename, header=TRUE, sep="\t")
-    currentData$resp2 <- ifelse(currentData$Attempts == "1", 1,0)
-    firstAttempts <- mean(currentData$resp2)
-    totalAttmpts <- mean(currentData$Attempts)
-    MedianAttmpts <- median(currentData$Attempts)
-    data1[i,0:5] <- c(rat, sessNum, firstAttempts,totalAttmpts,MedianAttmpts)
+    if(file.exists(filename)){
+      print(filename)
+      currentData <- read.csv(filename, header=TRUE, sep="\t")
+      currentData$resp2 <- ifelse(currentData$Attempts == "1", 1,0)
+      firstAttempts <- mean(currentData$resp2)
+      totalAttmpts <- mean(currentData$Attempts)
+      MedianAttmpts <- median(currentData$Attempts)
+      data1[i,0:5] <- c(rat, sessNum, firstAttempts,totalAttmpts,MedianAttmpts)}
     i=i+1
   }
 }
@@ -29,13 +30,21 @@ for (sessNum in sessList){
 data1 <- data.frame(data1) #making it into a data frame
 data1$ratID <- factor(data1$ratID) #making rat number a factor
 
-aggregate(data1$firstAttempt, by=list(data1$session), FUN='mean')
-aggregate(data1$firstAttempt, by=list(data1$session), FUN='sd')
+firstAttempts <- aggregate(data1$firstAttempt, by=list(data1$session), FUN='mean')
+SDs<-aggregate(data1$firstAttempt, by=list(data1$session), FUN='sd')
+totalAttempts<- aggregate(data1$meanAttempts, by=list(data1$session), FUN='mean')
+total_SDs <- aggregate(data1$meanAttempts, by=list(data1$session), FUN='sd')
 
-aggregate(data1$meanAttempts, by=list(data1$session), FUN='mean')
-aggregate(data1$meanAttempts, by=list(data1$session), FUN='sd')
+data2<-as.data.frame(firstAttempts)
+colnames(data2)[1] <- "session"
+colnames(data2)[2] <- "firstAttempts"
+data2$firstAttempts_SD <- SDs$x
+data2$totalAttempts <- totalAttempts$x
+data2$totalAttempts_sD <- total_SDs$x
 
-aggregate(data1$medianAttempts, by=list(data1$session), FUN='mean')
-aggregate(data1$medianAttempts, by=list(data1$session), FUN='sd')
 
 write.csv(data1, file="Group_Attempt_Data.csv")
+write.csv(data2, file="Mean_first_Attempts.csv")
+
+data3 <- subset(data1, ratID != list(105,109))
+aggregate(data3$firstAttempt, by=list(data3$session),FUN='mean')
